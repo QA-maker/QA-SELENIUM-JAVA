efd7a9c09eaa3f39773f749b2bfbbac2d0f4a886
CyberNet <cybsarynet@gmail.com>
2023-05-06 14:01:03 -0600

    Scroll Web Page and Scroll To Component with JavaScript

---
+++ src/ScrollJSExecutor.java
@@ -0,0 +1,33 @@
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class ScrollJSExecutor {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice");		
+		
+		//Scroll into WebPAge
+		JavascriptExecutor js= (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,500)");
+		
+		//ScrollTop into Component 
+		Thread.sleep(3000);
+		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+		
+		
+
+	}
+
+}
