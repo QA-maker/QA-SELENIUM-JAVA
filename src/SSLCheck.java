fd6c8bf11c081d354fe9ef1f736360ea9cbf3ab1
CyberNet <cybsarynet@gmail.com>
2023-05-09 14:47:57 -0600

    Take Screenshot code Selenium Java v 1.0

--- src/SSLCheck.java
+++ src/SSLCheck.java
@@ -1,14 +1,20 @@
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class SSLCheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//
+		//para configurar proxys cuando se necesite ingresar 
+		//examples 
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.getBrowserVersion();
+		Proxy proxy =new Proxy();
+		proxy.setHttpProxy("ipadrres:4444");
+		chromeOptions.setCapability("proxy",proxy);
 		chromeOptions.setAcceptInsecureCerts(true);
 		chromeOptions.addArguments("remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(chromeOptions);
--- src/selenium/frames/Work3Scroll.java
+++ src/selenium/frames/Work3Scroll.java
@@ -1,10 +1,15 @@
 package selenium.frames;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +17,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Work3Scroll {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
 		// TODO Auto-generated method stub
@@ -22,37 +27,61 @@ public class Work3Scroll {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		getScreenShoot(driver);
 		driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice");
-
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-
-		js.executeScript("window.scrollBy(0,700)");
-		// tr ROWS 11
 		
+		getScreenShoot(driver);
+		getElementsTable(driver);
+		getScreenShoot(driver);
 		
-//				 System.out.println(tables.findElements(By.tagName("tr")).size());
-		WebElement tables= driver.findElement(By.cssSelector(".left-align table[id='product']"));
-//		 System.out.println(tables.findElements(By.tagName("tr")).size());
-		 int rows=0;
-		 int column=0;
-		 for (int i=0; i<tables.findElements(By.tagName("th")).size(); i++) { //rows
+	}
+		
+public static void getElementsTable(WebDriver driver) throws IOException {
+	
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+
+	js.executeScript("window.scrollBy(0,700)");
+	// tr ROWS 11
+	
+	
+//			 System.out.println(tables.findElements(By.tagName("tr")).size());
+	WebElement tables= driver.findElement(By.cssSelector(".left-align table[id='product']"));
+	
+	
+//	 System.out.println(tables.findElements(By.tagName("tr")).size());
+	 int rows=0;
+	 int column=0;
+	 for (int i=0; i<tables.findElements(By.tagName("th")).size(); i++) { //rows
 			 
-			 System.out.println(tables.findElements(By.tagName("th")).get(i).getText());
-		 
+		 System.out.println(tables.findElements(By.tagName("th")).get(i).getText());
+		
+	 }
+		 for(int y=0; y<tables.findElements(By.tagName("td")).size(); y++) {//count columns
+		 System.out.println(tables.findElements(By.tagName("td")).get(y).getText());
+		
+//			 column++;
 		 }
-			 for(int y=0; y<tables.findElements(By.tagName("td")).size(); y++) {//count columns
-				 
-				 System.out.println(tables.findElements(By.tagName("td")).get(y).getText());
-//				 column++;
-			 }
-		 rows++;
-//			 break;
-		 }
-			
-			
-			
-			
-		 }
+	 rows++;
+	 
+//		 break;
+	 }
 
+	public static void getScreenShoot(WebDriver driver) throws IOException {
+		
+		File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(src, new File("C:\\Users\\isc_e\\eclipse-workspace\\Introduction\\src\\selenium\\frames\\ScrreenShot//screenshot.png"));
+		
+		
+	}
+
+		
+		
+		
+		
+}
+
+
+		
 
 
